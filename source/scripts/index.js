ee716d47a71db877973b44a0a0aabea62eeabd8d
Татьяна <tanya.karpova.ry@gmail.com>
2024-02-02 22:12:27 +0300

    Initial commit

---
+++ source/scripts/index.js
@@ -0,0 +1,21 @@
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+
+const mapAddress = document.querySelector('.address__map');
+const page = document.querySelector('.page--nojs');
+
+mapAddress.classList.remove('address__map--nojs');
+page.classList.remove('page--nojs');
